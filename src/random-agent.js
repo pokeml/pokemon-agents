4cca509577781c90e18afd347a6e8fda709999e3
Christopher Wolff <cfw13@duke.edu>
2018-11-29 02:29:01 -0500

    Enable seeding

--- src/random-agent.js
+++ src/random-agent.js
@@ -1,11 +1,19 @@
 'use strict';
 
-const _ = require('underscore');
+const seedrandom = require('seedrandom');
 
 /**
  * An agent that chooses actions uniformly at random.
  */
 class RandomAgent {
+    /**
+     * @param {number} [seed]
+     */
+    constructor(seed = null) {
+        this.seed = seed;
+        this.random = seedrandom(seed);
+    }
+
     /**
      * Choose an action.
      *
@@ -14,7 +22,17 @@ class RandomAgent {
      * @return {string}
      */
     act(state, actions) {
-        return _.sample(actions);
+        return this._sample(actions);
+    }
+
+    /**
+     * Sample a random element from an array.
+     *
+     * @param {Array} arr
+     * @return {Object}
+     */
+    _sample(arr) {
+        return arr[Math.floor(this.random() * arr.length)];
     }
 }
 
