2b37233c9eef0c52d6ef36b6be8025949ebbf213
Christopher Wolff <cfw13@duke.edu>
2018-12-07 20:24:40 -0500

    Create encoder

--- src/agents/test-agent.js
+++ src/agents/test-agent.js
@@ -8,26 +8,8 @@ const Encoder = require('../core/encoder');
 const TrackingAgent = require('../core/tracking-agent');
 const toId = require('../../utils/utils').toId;
 
-/**
- * Create a dictionary that maps each element of the given array to its index.
- *
- * @param {Array} arr
- * @return {Object}
- */
-const createDict = function(arr) {
-    if (!arr.every((element) => typeof element === 'string')) {
-        throw new Error('Array must only contain strings');
-    }
-    const uArr = _.uniq(arr);
-    const dict = {};
-    for (let i = 0; i < uArr.length; i++) {
-        dict[uArr[i]] = i;
-    }
-    return dict;
-};
-
 const pokemon = [
-    'tapukoko', 'landorustherian', 'toxapex', 'serperior', 'celesteela', 'medicham',
+    'tapukoko', 'landorus', 'landorustherian', 'toxapex', 'serperior', 'celesteela', 'medicham',
     'medichammega', 'ninetalesalola', 'magearna', 'zygarde', 'mawile', 'mawilemega', 'manaphy',
 ];
 const moves = [
@@ -38,10 +20,9 @@ const moves = [
     'Earthquake', 'U-turn', 'Stealth Rock', 'Hidden Power Ice',
     'Leaf Storm', 'Leech Seed', 'Substitute', 'Hidden Power Fire',
 ];
-
-const dict = {
-    'pokemon': createDict(pokemon),
-    'move': createDict(moves),
+const vocab = {
+    'pokemon': pokemon,
+    'move': moves,
 };
 
 /**
@@ -53,7 +34,7 @@ class TestAgent extends TrackingAgent {
      */
     constructor() {
         super();
-        this.encoder = new Encoder(dict);
+        this.encoder = new Encoder(vocab, 'gen7ou');
     }
 
     /**
@@ -73,7 +54,15 @@ class TestAgent extends TrackingAgent {
         const myActive = state.mySide.active[0].id;
         const yourActive = state.yourSide.active[0].id;
         const myPokemon = state.request.side.pokemon.map((pokemon) => toId(pokemon.ident.substring(3)));
-        const moves = state.request.active ? state.request.active[0].moves.map((m) => m.id) : [null, null, null, null];
+        const moves = state.request.active ? state.request.active[0].moves.map((m) => m.move) : [null, null, null, null];
+
+        const features = [
+            ['my_active', 'pokemon', myActive],
+            ['your_active', 'pokemon', yourActive],
+            ['team', 'pokemon', myPokemon],
+            ['moves', 'move', moves],
+        ];
+        console.log(this.encoder.encode(features));
 
         return {
             'myActive': myActive,
--- src/core/encoder.js
+++ src/core/encoder.js
@@ -1,16 +1,16 @@
 'use strict';
 
-const loadDict = () => null;
+const _ = require('underscore');
 
 /**
- * @typedef {Object} Dict
- * @property {?Object} pokemon
- * @property {?Object} move
- * @property {?Object} ability
- * @property {?Object} item
- * @property {?Object} sideCondition
- * @property {?Object} weather
- * @property {?Object} pseudoWeather
+ * @typedef {Object} Vocab
+ * @property {?Array} pokemon
+ * @property {?Array} move
+ * @property {?Array} ability
+ * @property {?Array} item
+ * @property {?Array} sideCondition
+ * @property {?Array} weather
+ * @property {?Array} pseudoWeather
  */
 
 /**
@@ -18,38 +18,133 @@ const loadDict = () => null;
  */
 class Encoder {
     /**
-     * @param {string} tier
-     * @param {Dict} dict
+     * @param {Vocab} [vocab]
+     * @param {string} [tier]
      */
-    constructor(tier, dict = {}) {
+    constructor(vocab, tier) {
         this.tier = tier;
         this.dictIds = [
             'pokemon', 'move', 'ability', 'item', 'sideCondition', 'weather', 'pseudoWeather',
         ];
         this.dict = {};
         for (const id of this.dictIds) {
-            this.dict = id in dict ? dict[id] : loadDict(id, tier);
+            this.dict[id] = (id in vocab) ? this._createDict(vocab[id]) : this._loadDict(id, tier);
         }
+    }
+
+    /**
+     * Create an ordered mapping from each element of the given array to its index.
+     *
+     * @param {Array} arr
+     * @return {Map}
+     */
+    _createDict(arr) {
+        if (!arr.every((element) => typeof element === 'string')) {
+            throw new Error('Array must only contain strings');
+        }
+        const uArr = _.uniq(arr);
+        const map = new Map();
+        for (let i = 0; i < uArr.length; i++) {
+            map.set(uArr[i], i);
+        }
+        return map;
+    };
+
+    /**
+     * Load a default dictionary.
+     *
+     * @param {string} id
+     * @return {Object}
+     */
+    _loadDict(id) {
+        return null;
+    }
+
+    /**
+     * Create a single feature map from a set of features, each of some respective type.
+     *
+     * If the type is 'number', the feature is expected to be a number and will be added to the
+     * vector as is. Otherwise, the type must be a valid dictId (see this.dictIds), in which case
+     * value has to be either a valid value of that type or an array of valid values of that types.
+     *
+     * The features in the resulting map are ordered in the same way as they are in the given
+     * array.
+     *
+     * @param {Array} features - Each element has the form [name, type, value]. Each name must be
+     *     unique.
+     * @return {Map} - Each key has the form "name" for numeric features and "name:category" for
+     *     categorical features. Each corresponding value is the encoding of the given value.
+     */
+    encode(features) {
+        const map = new Map();
+        for (const [name, type, value] of features) {
+            if (type === 'number') {
+                map.set(name, value);
+            } else {
+                if (value instanceof Array) {
+                    for (const [index, val] of value.entries()) {
+                        for (const [key, encoding] of this._oneHot(type, val).entries()) {
+                            map.set(`${name}[${index}]:${key}`, encoding);
+                        }
+                    }
+                } else {
+                    for (const [key, encoding] of this._oneHot(type, value).entries()) {
+                        map.set(`${name}:${key}`, encoding);
+                    }
+                }
+            }
+        }
+        return map;
+    }
+
+    /**
+     * Encode a categorical entity of some type.
+     *
+     * If the entity is null or undefined, all entries will be zero.
+     *
+     * @param {string} type
+     * @param {string} value
+     * @param {number} onValue
+     * @param {number} offValue
+     * @return {Map}
+     */
+    _oneHot(type, value, onValue = 1, offValue = 0) {
+        if (type == null || !(this.dictIds.includes(type))) {
+            throw new Error(`Invalid type: ${type}`);
+        }
+        const map = new Map();
+        for (const category of this.dict[type].keys()) {
+            map.set(category, offValue);
+        }
+        if (value != null) {
+            map.set(value, onValue);
+        }
+        return map;
+    }
+
+    /**
+     * Get the entity to index mapping for a dictionary of some type.
+     *
+     * @param {string} type
+     * @return {Object}
+     */
+    getDictionary(type) {
+        return this.dict[type];
     }
 
     /**
      * Get the index of a given entity in a specified dictionary.
      *
      * @param {string} type
-     * @param {string} entity
-     * @return {Array}
+     * @param {string} value
+     * @return {number}
      */
-    index(type, entity) {
-        if (type == null || !(type in this.dictIds)) {
-            throw new Error(`Invalid type: ${type}`);
+    _index(type, value) {
+        if (value == null || !(value in this.dict[type])) {
+            throw new Error(`Entity ${value} not in dictionary ${type}`);
         }
-        if (!(type in this.dict)) {
-            throw new Error(`No dictionary for type: ${type}`);
-        }
-        if (entity == null || !(entity in this.dict[type])) {
-            throw new Error(`Invalid entity: ${entity}`);
-        }
-        return this.dict[type][entity];
+
+        return this.dict[type].get(value);
     }
 }
 
