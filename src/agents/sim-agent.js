2c4fa1a3f95ef7bda168fa755258b1508e230c39
Christopher Wolff <cwolff98@gmail.com>
2019-04-03 18:12:16 -0400

    Add empty sim-agent

---
+++ src/agents/sim-agent.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const seedrandom = require('seedrandom');
+const Agent = require('../core/agent');
+
+/**
+ * An agent that chooses actions based on forward simulation.
+ */
+class SimAgent extends Agent {
+    /**
+     * @param {number} [seed]
+     */
+    constructor(seed = null) {
+        super();
+        this.seed = seed;
+        this.random = seedrandom(seed);
+    }
+
+    /**
+     * @override
+     */
+    act(actionSpace, observation, reward, done) {
+        return this._sample(actionSpace);
+    }
+
+    /**
+     * @override
+     */
+    reset() {
+        this.random = seedrandom(this.seed);
+    }
+
+    /**
+     * Sample a random element from an array.
+     *
+     * @param {Array} arr
+     * @return {Object}
+     */
+    _sample(arr) {
+        return arr[Math.floor(this.random() * arr.length)];
+    }
+}
+
+module.exports = SimAgent;
