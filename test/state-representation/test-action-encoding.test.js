1398bc8097528b5d90f488f2b182bb5d681dd6d1
niklas <niklas.riewald@gmail.com>
2019-04-09 22:32:40 +0200

    Replace action encoding with 1 dimensional representation

--- test/state-representation/test-action-encoding.test.js
+++ test/state-representation/test-action-encoding.test.js
@@ -3,43 +3,33 @@ const PokemonEnvActions = require('pokemon-env/src/actions');
 
 
 test(
-    'test switch action encoding', () => {
-        expect(
-            encoding.encodeActions(new PokemonEnvActions.SwitchAction(1), 5)).toEqual(
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
+    'test switch action decoding', () => {
+        expect(encoding.decodeAction(12)).toEqual(
+            new PokemonEnvActions.SwitchAction(1)
         );
     }
 );
 
 test(
-    'test move action encoding', () => {
-        expect(
-            encoding.encodeActions(
-                new PokemonEnvActions.MoveAction(4, {mega: 0, zmove: 0}))
-        ).toEqual(
-            [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
+    'test switch move decoding', () => {
+        expect(encoding.decodeAction(2)).toEqual(
+            new PokemonEnvActions.MoveAction(3, {mega: 0, zmove: 0})
         );
     }
 );
 
 test(
-    'test move action mega evo encoding', () => {
-        expect(
-            encoding.encodeActions(
-                new PokemonEnvActions.MoveAction(2, {mega: 1, zmove: 0}))
-        ).toEqual(
-            [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0]
+    'test switch move decoding mega', () => {
+        expect(encoding.decodeAction(4)).toEqual(
+            new PokemonEnvActions.MoveAction(1, {mega: 1, zmove: 0})
         );
     }
 );
 
 test(
-    'test move action zmove encoding', () => {
-        expect(
-            encoding.encodeActions(
-                new PokemonEnvActions.MoveAction(1, {mega: 0, zmove: 1}))
-        ).toEqual(
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+    'test switch move decoding zmove', () => {
+        expect(encoding.decodeAction(11)).toEqual(
+            new PokemonEnvActions.MoveAction(4, {mega: 0, zmove: 1})
         );
     }
 );
